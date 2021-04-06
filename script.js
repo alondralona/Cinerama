97a795c55737c46a901df671ec00b18cad7ca3f6
austinescalante <60208327+austinescalante@users.noreply.github.com>
2021-04-05 21:34:52 -0700

    Add onclick reference for each movie

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 const APIKEY= "a9abe32e301cc47ca49435473c6ae4a2";
-const APIURL = "https://api.themoviedb.org/3/movie/5/recommendations?api_key=a9abe32e301cc47ca49435473c6ae4a2&language=en-US&page=2";
+const APIURL = "https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=a9abe32e301cc47ca49435473c6ae4a2&language=en-US&page=1";
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 
 
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=a9abe32e301cc47ca49435473c6ae4a2&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key="+ APIKEY + "&query=";
 
 const main = document.getElementById('main');
 const search = document.getElementById('search');
 const form = document.getElementById('searchForm');
 
-findMovies(APIURL);
+
+findMovies();
 
 async function findMovies(url){
     const response = await fetch(url);
     const data = await response.json();
 
     console.log(data);
-/*
-    data.results.forEach((movie) => {
-        const img = document.createElement('img');
-        img.src = IMGPATH + movie.poster_path;
-
-        document.body.appendChild(img);
-        
-    });
-    */
     displaySearch(data.results);
-
 }
 
 function displaySearch(movies){
@@ -43,7 +34,32 @@ function displaySearch(movies){
         elementMovie.innerHTML = 
                `<img src = "${IMGPATH + poster_path}" 
                 alt =""
-                />
+                data-movie-id=${movie.id}/>
+                <div class = "movie-info">
+                    <h3>${title}</h3>
+                </div>`
+            ;
+        main.appendChild(elementMovie);
+
+    });
+}
+
+
+function displaySearchRec(movies){
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+
+        const {poster_path, title} = movie;
+
+        const elementMovie = document.createElement('div');
+
+        elementMovie.classList.add('movie');
+
+        elementMovie.innerHTML = 
+               `<img src = "${IMGPATH + poster_path}" 
+                alt =""
+                data-movie-id=${movie.id}/>
                 <div class = "movie-info">
                     <h3>${title}</h3>
                 </div>`
@@ -66,6 +82,37 @@ form.addEventListener("submit", (x) =>{
 
 
 })
+function generateUrl(path){
+    const url = `https://api.themoviedb.org/3${path}?api_key=a9abe32e301cc47ca49435473c6ae4a2&language=en-US&page=1`;
+    return url;
+}
+
+document.onclick = function(event){
+    const target = event.target
+    if(target.tagName.toLowerCase() === 'img'){
+    const movieId = target.dataset.movieId;
+    console.log('Event: ',event)
+    console.log(target.dataset.movieId);
+
+    const path = `/movie/${movieId}recommendations`;
+    const url = generateUrl(path);
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) =>{
+            console.log('Recommendations',data);
+            displaySearchRec(data.results);
+        })
+        .catch((error) =>{
+            console.log('Error:', error);
+        });   
+           
+    }
+
+   
+}
+
+
 
 
 
